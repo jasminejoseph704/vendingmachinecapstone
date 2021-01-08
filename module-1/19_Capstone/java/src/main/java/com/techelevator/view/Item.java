4eea1f71879d7e3f5c06dcbdbd0337034bdf30f3
Jasmine Joseph <jasmineljoseph2000@gmail.com>
2021-01-07 20:27:23 -0500

    item class

---
+++ module-1/19_Capstone/java/src/main/java/com/techelevator/view/Item.java
@@ -0,0 +1,33 @@
+package com.techelevator.view;
+
+/**
+ * 
+ * public final class Item {
+
+	
+	private final String slotId;
+	private final String productName;
+	private final double price;
+	
+	public item (String slotId, String productName, double price) {
+		this.slotId = slotId;
+		this.productName = productName;
+		this.price = price;
+	}
+
+	public String getSlotId() {
+		return slotId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+
+}
+
+ */
